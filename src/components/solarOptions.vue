7b9969671d4ec5dd0ef0f29d4db4c65dbae6a61b
anagh-sheth <53011632+anagh-sheth@users.noreply.github.com>
2021-07-11 18:38:43 -0400

    css changes

--- src/components/solarOptions.vue
+++ src/components/solarOptions.vue
@@ -26,31 +26,51 @@ export default {
 name:"solarOptions",
 props:['energy','units','ppe','pps']
 }
-</script>
 
 <style scoped>
 #main{
     text-align: center;
+    background-color: rgb(204, 252, 203);
+    padding: 10px;
 }
 #container{
   display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    justify-content: space-around;
-  
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-around;
+} 
+#flexItem1{
+  padding: 20px ;
+  border-style: outset;
+  border-width: 2px; 
+  border-color: black;
+  background-color: rgb(150, 230, 179);
+  border-radius: 25px;
 }
-#flexItem{
+
+#flexItem2{
+  padding: 20px ;
+  border-style: outset;
+  border-width: 2px; 
+  border-color: black;
+  background-color: rgb(150, 230, 179);
+  border-radius: 25px;
+}
+
+#flexItem3{
   padding: 10px ;
 }
 #title{
   padding: 10px;
 }
 .secondBox {
-    background: white;
+    background: rgb(150, 230, 179);
+
 }
 .dataBox{
     font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
     font-weight: bold;
     font-size: 300%;
+
 }
 </style>
